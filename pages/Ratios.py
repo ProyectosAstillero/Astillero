b6e3a1fc00f8dd6c5772c010a23dd915bf042866
ProyectosAstillero <controldeproyectosastillero@gmail.com>
2025-06-19 07:54:26 -0500

    Add files via upload

--- pages/Ratios.py
+++ pages/Ratios.py
@@ -105,10 +105,10 @@ df_ratio['DiscoxAcero'] = df_ratio['Discos(pz)']/df_ratio['Peso(Tn)']
 df_ratio.fillna(0, inplace=True)
 
 #Se elimina la categoría de : "PG" , "SISTEMAS AUXILIARES"
-df_ratio = df_ratio[df_ratio['Categoría'].isin(['CASCO','ADITAMENTO','PANGA','VELA I CARENA','VELA I MODULO'
-])]
+categ = ['CASCO','ADITAMENTO','PANGA','VELA I CARENA','VELA I MODULO']
+df_ratio = df_ratio[df_ratio['Categoría'].isin(categ)]
 
-selector_categoria = st.sidebar.selectbox("Seleccione categoria:", ['CASCO','ADITAMENTO','PANGA','VELA I CARENA','VELA I MODULO'])
+selector_categoria = st.sidebar.selectbox("Seleccione categoria:", categ)
 
 #Se filtra la categoría
 df_ratio_filtrado = df_ratio[df_ratio['Categoría'].isin([selector_categoria])]
